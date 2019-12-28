0ee174e058cdb411da06e540f7c0ce600cdb0310
grayd4 <32846772+grayd4@users.noreply.github.com>
2019-12-27 21:56:58 -0500

    Added pulling of photos and time differentiation of time folders.

--- scraper.py
+++ scraper.py
@@ -1,13 +1,22 @@
-#import requests
 import urllib.request
 import time
-#from bs4 import BeautifulSoup
+from datetime import datetime
 
+# Apgar Mt. Northeast View NPS Webcam
 url = 'https://www.nps.gov/webcams-glac/aplocam.jpg'
-#response = requests.get(url)
-#soup = BeautifulSoup(response.text, "html.parser")
-#imgTag = soup.findall('img')[0]
-#link = 
 
-urllib.request.urlretrieve(url, 'aplocam' + str(time.time()) + '.jpg')
-time.sleep(60)
+if __name__ == "__main__":
+
+    # Returns the hour (0-24). Meant to be used in the Pacific timezone
+    timeLocal = time.localtime(time.time()).tm_hour
+
+    nameString = ''
+
+    # If a night photo - from 8 to 4 am
+    if timeLocal > 20 or timeLocal < 4:
+        nameString = 'Night/aplocam'
+    else:
+        nameString = 'Day/aplocam'
+
+    # Save the webcam capture in the desired location
+    urllib.request.urlretrieve(url, nameString + str(time.time()) + '.jpg')
