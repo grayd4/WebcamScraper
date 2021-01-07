3c4feaf591359648aa10136598ae970a4ce452b3
Daniel <grayd4@spu.edu>
2021-01-07 15:11:29 -0800

    Changed bat files to sh files and python commands to work easily in ubuntu

---
+++ runWebcamScraper.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+cd ~/Documents/Working/Webcam_Scraper/WebcamScraper
+python3 scraper.py -c aplocam -sub glacier
+python3 scraper.py -c logan1 -sub glacier
+python3 scraper.py -c logan2 -sub glacier
+python3 scraper.py -c loganpark -sub glacier
+python3 scraper.py -c mcdcam2 -sub glacier
+python3 scraper.py -c mcdonaldaxis -sub glacier
+python3 scraper.py -c mgcam -sub glacier
+python3 scraper.py -c mgptz -sub glacier
+python3 scraper.py -c middleforkcam -sub glacier
+python3 scraper.py -c smcam -sub glacier
+python3 scraper.py -c smptz -sub glacier
+python3 scraper.py -c tmcam -sub glacier
+python3 scraper.py -c twomedptz -sub glacier
+python3 scraper.py -c flathead -sub glacier
+
+python3 scraper.py -c current_ridgecam -u https://www.nps.gov/webcams-olym/ -sub olympic
+python3 scraper.py -c kalaloch -u http://pixelcaster.com/dnc-kalaloch/ -sub olympic
+python3 scraper.py -c netcam -u http://webcam.forkswa.com/webcam2/ -sub olympic
+python3 scraper.py -c olym -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub olympic
+
+python3 scraper.py -c current_ylake -u https://volcanoes.usgs.gov/vsc/captures/yellowstone/ -sub yellowstone
+python3 scraper.py -c washburn_ne -u https://www.nps.gov/webcams-yell/ -sub yellowstone
+python3 scraper.py -c washburn_sw -u https://www.nps.gov/webcams-yell/ -sub yellowstone
+python3 scraper.py -c mammoth_parade -u https://www.nps.gov/webcams-yell/ -sub yellowstone
+python3 scraper.py -c mammoth_electric -u https://www.nps.gov/webcams-yell/ -sub yellowstone
+python3 scraper.py -c mammoth_arch -u https://www.nps.gov/webcams-yell/ -sub yellowstone
+
+python3 scraper.py -c mountain -u https://www.nps.gov/webcams-mora/ -sub rainier
+python3 scraper.py -c west -u https://www.nps.gov/webcams-mora/ -sub rainier
+python3 scraper.py -c tatoosh -u https://www.nps.gov/webcams-mora/ -sub rainier
+
+python3 scraper.py -c yosfalls -u https://pixelcaster.com/yosemite/webcams/ -sub yosemite
+python3 scraper.py -c ahwahnee2 -u https://pixelcaster.com/yosemite/webcams/ -sub yosemite
+python3 scraper.py -c sentinel -u https://pixelcaster.com/yosemite/webcams/ -sub yosemite
+python3 scraper.py -c turtleback -u https://pixelcaster.com/yosemite/webcams/ -sub yosemite
+python3 scraper.py -c yoselarge -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub yosemite
+
+python3 scraper.py -c grsm -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub great_smoke_mountain
+python3 scraper.py -c NEON.D07.GRSM.DP1.00033 -u https://phenocam.sr.unh.edu/data/latest/ -sub great_smoke_mountain
+python3 scraper.py -c gsng -u https://www.air-resource.net/GRSMNFGap/ -sub great_smoke_mountain
+python3 scraper.py -c grpk -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub great_smoke_mountain
+
+python3 scraper.py -c camerasinnott -u https://www.nps.gov/webcams-crla/ -sub crater_lake
+
+python3 scraper.py -c alpine -u https://www.nps.gov/webcams-dena/ -sub denali
+
+python3 scraper.py -c kawuneeche_valley -u https://www.nps.gov/webcams-romo/ -sub rocky_mnp
+python3 scraper.py -c glacier_basin -u https://www.nps.gov/webcams-romo/ -sub rocky_mnp
+python3 scraper.py -c longs_peak -u https://www.nps.gov/webcams-romo/ -sub rocky_mnp
+python3 scraper.py -c alpine_visitor_center -u https://www.nps.gov/webcams-romo/ -sub rocky_mnp
+
+python3 scraper.py -c pinnacles_800 -u https://www.nps.gov/webcams-shen/ -sub shenandoah
+python3 scraper.py -c bvc_1024x576 -u https://www.nps.gov/webcams-shen/ -sub shenandoah
+python3 scraper.py -c bvc2 -u https://www.nps.gov/webcams-shen/ -sub shenandoah
+
+python3 scraper.py -c camera -u https://www.nps.gov/webcams-zion/ -sub zion
+
+python3 scraper.py -c camera1 -u https://www.wrh.noaa.gov/images/fgz/webcam/ -sub flagstaff
+
+python3 scraper.py -c bibe -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub big_bend
+
+python3 scraper.py -c oak_canopy -u https://www.nps.gov/webcams-seki/ -sub sequoia_kings
+python3 scraper.py -c seki -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub sequoia_kings
+
+python3 scraper.py -c ml_webcam -u https://www.nps.gov/webcams-lavo/ -sub lassen
+python3 scraper.py -c kyvc_webcam1 -u https://www.nps.gov/webcams-lavo/ -sub lassen
+
+python3 scraper.py -c iskywebcam -u https://www.nps.gov/cany/learn/photosmultimedia/images/ -sub canyonlands
+
+python3 scraper.py -c sr2 -u https://www.nps.gov/webcams-blca/ -sub black_canyon_gunnison
+python3 scraper.py -c srvc -u https://www.nps.gov/webcams-blca/ -sub black_canyon_gunnison
+
+python3 scraper.py -c sprucetree000M -u https://www.nps.gov/webcams-meve/ -sub mesa_verde
+
+python3 scraper.py -c camera0 -u https://www.nps.gov/webcams-gumo/ -sub guadalupe
+python3 scraper.py -c gumo -u https://www.nps.gov/webcams-gumo/ -sub guadalupe
+
+python3 scraper.py -c RMD_VC_01 -u https://www.nps.gov/webcams-sagu/ -sub saguaro
+
+python3 scraper.py -c eye -u https://www.nps.gov/webcams-acad/ -sub acadia
+
+python3 scraper.py -c thro -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub theo_roos
+
+python3 scraper.py -c BartlettLagoon -u https://www.nps.gov/webcams-glba/ -sub glacier_bay
+python3 scraper.py -c LowerBay -u https://www.nps.gov/webcams-glba/ -sub glacier_bay
+python3 scraper.py -c BartlettDock -u https://www.nps.gov/webcams-glba/ -sub glacier_bay
+
+python3 scraper.py -c grte -u https://www.nps.gov/featurecontent/ard/webcams/images/ -sub grand_teton
+python3 scraper.py -c webcam -u http://www.americanalpineclub.org.php56-21.dfw3-2.websitetestlink.com/images/gtcr_webcam/ -sub grand_teton
+
+python3 scraper.py -c HaleSummitCamCrater -u https://www.nps.gov/webcams-hale/ -sub haleakala
+
+python3 scraper.py -c KWcam/images/M -u https://volcanoes.usgs.gov/observatories/hvo/cams/ -sub hawaii_volcanoes
+python3 scraper.py -c F1cam/images/M -u https://volcanoes.usgs.gov/observatories/hvo/cams/ -sub hawaii_volcanoes
+
+python3 scraper.py -c thimage -u https://www.nps.gov/webcams-isro/ -sub isle_royale
+python3 scraper.py -c image -u https://www.nps.gov/webcams-isro/ -sub isle_royale
+
+python3 scraper.py -c bear_glacier -u https://pvs.nupointsystems.com/latest2.php?pass=%F3uq%2C%F7%85c%D7%2A0%9D%95%5D%05%00 -sub kenai_fjords
